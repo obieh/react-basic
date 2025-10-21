1455004fdad3889553303ed02711f0a59ee99968
obieh <o_amadic@yahoo.com>
2025-10-22 00:45:53 +0100

    exported home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-  
+import Home from './pages/Home'   
 
 function App() {
   return (
     <>
-      
+     <Home/>
     </>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,20 @@
+import MovieCard from '../components/MovieCard'
+
 function Home() {
 
   const movies = [
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010
-    },
-    {
-      title: 'The Matrix',
-      director: 'The Wachowskis',
-      year: 1999
-    },
-    {
-      title: 'Interstellar',
-      director: 'Christopher Nolan',
-      year: 2014
-    }
+    { id: 1, title: 'Inception', director: 'Christopher Nolan', year: 2010 },
+    { id: 2, title: 'The Matrix', director: 'The Wachowskis', year: 1999},
+    { id: 3, title: 'Interstellar', director: 'Christopher Nolan', year: 2014 }
   ] 
+
   return (
     <div className="home">
+        <div className="movies-grid">
+            {movies.map((movie) => (<MovieCard movie={movie} key={movie.id} />))}
+        </div>
     </div>
   )
 }
+
+export default Home
