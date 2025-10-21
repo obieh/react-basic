47b909b5b1028ce1ee629507ddea5d09650698fb
obieh <o_amadic@yahoo.com>
2025-10-21 23:16:13 +0100

    added moviecard component

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+function MovieCard({movie}) {
+
+  function onFavoriteClick() {
+    alert("Favorite clicked!")
+  }
+
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={`${movie.title} Poster`} />
+      </div>
+      <div className="movie-overlay">
+        <button className="favorite-btn" onClick={onFavoriteClick}>
+            ❤️
+        </button>
+      </div>
+      <div className="movie-details">
+        <h2 className="movie-title">{movie.title}</h2>
+        <p className="movie-year">{movie.year}</p>
+      </div>
+
+    </div>
+  );
+}
+
+
+export default MovieCard;
