10c6b5fa36cae2646a419221a17a4864fb1b8909
obieh <o_amadic@yahoo.com>
2025-10-23 15:49:36 +0100

    modified favorite

--- frontend/src/pages/Favorite.jsx
+++ frontend/src/pages/Favorite.jsx
@@ -1,10 +1,10 @@
 function Favorite() {
   return (
-    <div className="favorites-empty">
+    <div className="favorite-empty">
       <h2>No Favorite Movie Yet!</h2>
       <p>Start adding movies to Your favorites and they will show up here </p>
     </div>
-  );
+  )
 }
 
 export default Favorite
