8a3bfb679b85bfc81f847b6bc3c8385db8b28dac
obieh <o_amadic@yahoo.com>
2025-10-24 01:59:19 +0100

    adjusted date moviecard

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ function MovieCard({movie}) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={`${movie.title} Poster`} />
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} Poster`} />
       </div>
       <div className="movie-overlay">
         <button className="favorite-btn" onClick={onFavoriteClick}>
@@ -19,7 +19,7 @@ function MovieCard({movie}) {
       </div>
       <div className="movie-details">
         <h2 className="movie-title">{movie.title}</h2>
-        <p className="movie-year">{movie.year}</p>
+        <p className="movie-year">{movie.release_date?.split("-")[0]}</p>
       </div>
 
     </div>
