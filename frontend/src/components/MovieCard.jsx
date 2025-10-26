7e3475745504426059ccc83c5baba56d53d3132c
obieh <o_amadic@yahoo.com>
2025-10-26 19:31:44 +0100

    updated moviecard

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,38 +1,30 @@
 import "../css/MovieCard.css"
-import { useMovieContext } from "../contexts/MovieContext";  
+import { useMovieContext } from "../contexts/MovieContext"
 
 function MovieCard({movie}) {
-  const { addFavorite, removeFavorite, isFavorite } = useMovieContext(); 
-  const favorite = isFavorite(movie.id); 
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-  function onFavoriteClick() {
-    //e.preventDefault();
-    if (favorite) {
-        removeFavorite(movie.id);
-    } else {
-        addFavorite(movie);
-    } 
-  }
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
+    }
 
-
-  return (
-    <div className="movie-card">
-      <div className="movie-poster">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} Poster`} />
-      </div>
-      <div className="movie-overlay">
-        <button type="button" className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
-            ❤️
-        </button>
-      </div>
-      <div className="movie-details">
-        <h2 className="movie-title">{movie.title}</h2>
-        <p className="movie-year">{movie.release_date}</p>
-      </div>
-
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date?.split("-")[0]}</p>
+        </div>
     </div>
-  );
 }
 
-
-export default MovieCard;
+export default MovieCard
